78311e35b833d6745587509e21864c4899c10fd4
EEthanZZ <zycc2727@gmail.com>
2023-11-09 22:31:18 +1100

    pipeline update to fit sonar gate quality

--- src/main/resources/pipeline_practice.groovy
+++ src/main/resources/pipeline_practice.groovy
@@ -25,9 +25,32 @@ pipeline {
             }
         }
 
-        stage('UNIT TESTS') {
+        stage('TEST') {
             steps {
                 sh 'mvn test'
+            }
+        }
+        stage ('Checkstyle Analysis') {
+            steps {
+                sh 'mvn checkstyle:checkstyle'
+            }
+        }
+        stage ('Sonar Analysis') {
+            environment {
+                scannerHome = tool 'sonar4.7'
+            }
+            steps {
+                withSonarQubeEnv('sonar') {
+                    sh '''${scannerHome}/bin/sonar-scanner
+                    -Dsonar.projectKey=vprofile \
+                    -Dsonar.projectName=vprofile \
+                    -Dsonar.projectVersion=1.0 \
+                    -Dsonar.sources=src/ \
+                    -Dsonar.java.binaries=target/test-classes/com/visualpathit/account/controllerTest \
+                    -Dsonar.junit.reportPath=target/surefile-reports \
+                    -Dsonar.coverage.jacoco.reportPath=target/jacoco/jacoco.exec \
+                    -Dsonar.checkstyle.reportPaths=target/checkstyle-report.xml'''
+                }
             }
         }
     }
